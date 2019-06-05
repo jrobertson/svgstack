8e777ca3edbb145955ca01583bdd6821ce9a4dd7
James Robertson <james@jamesrobertson.eu>
2019-06-05 11:01:34 +0100

    feature: Added public method *import* which allows importing a raw svgstack document.

--- lib/svgstack.rb
+++ lib/svgstack.rb
@@ -5,6 +5,7 @@
 # description: A 1st attempt at creating a stack using SVG
 
 require 'victor'
+require 'dynarex'
 
 
 module RegGem
@@ -26,14 +27,9 @@ class SvgStack
   attr_accessor :css
 
   def initialize(data=nil, x: 0,  y: 0, width: 150, height: nil)
-
-    boxes = data if data.is_a? Array
     
-    height ||= (boxes.length * 50) + 50    
-
-    @svg = Victor::SVG.new viewBox: [x, y, width, height].join(' ')
-    @boxwidth = width
-    build boxes
+    
+    @x, @y, @width, @height = x, y, width, height
     
     @css = "
     rect {
@@ -43,38 +39,90 @@ class SvgStack
     text { fill: blue}
 "
 
+    if data then
+      @boxes = data       
+      @doc = render()
+    end
+
+
+  end
+  
+  def import(s)
+    
+    s2 = s.slice(/<\?svgstack [^>]*\?>/)
+
+    if s2 then
+      
+      attributes = %w(delimiter id schema).inject({}) do |r, keyword|
+        found = s2[/(?<=#{keyword}=['"])[^'"]+/]
+        found ? r.merge(keyword.to_sym => found) : r
+      end
+      
+    end
+    
+    @dx = Dynarex.new s.sub(/svgstack/, 'dynarex') 
+    @boxes = @dx.to_h.to_a.reverse
+    @doc = render()
+
+  end
+  
+  def render()
+    
+    @height ||= (@boxes.length * 50) + 50    
+
+    @svg = Victor::SVG.new viewBox: [@x, @y, @width, @height].join(' ')
+    
+    build @boxes
+    
   end
 
-  def save(file='untitled')
-    
-    @svg.save file
-    
-    #inject the css    
-    
-    s = File.read file
-    s2 = s.lines.insert 7, css_code()
-    File.write file, s2.join
+  def save(file='untitled')    
+    File.write file, @doc
+  end
+  
+  def to_svg()
+    @doc
   end
 
   private
+  
+  def add_box(label, y)
+    
+    boxwidth = @width
+    
+    @svg.rect(x: 0, y: y, width: boxwidth, height: 40)
+    @svg.text label, {x: boxwidth / 2, y: y+20, font_family: 'arial', 
+              font_weight: 'bold', font_size: 20,  
+              :"dominant-baseline" => "middle", :"text-anchor" => "middle"}    
+  end
 
-  def build(labels)
+  def build(layers)
 
     y = 0
 
-    labels.reverse.each do |label|
+    layers.reverse.each do |label, url|
 
       @svg.g do
-        @svg.rect x: 0, y: y, width: @boxwidth, height: 40
-        @svg.text label, {x: @boxwidth / 2, y: y+20, font_family: 'arial', 
-                  font_weight: 'bold', font_size: 20,  
-                  :"dominant-baseline" => "middle", :"text-anchor" => "middle"}
+        
+        if url then
+          
+          @svg.a(href: url) do 
+            add_box(label, y)
+          end
+          
+        else
+          add_box(label, y)
+        end
 
       end
 
       y+=50
 
     end
+    
+    #inject the css    
+    
+    @svg.render.lines.insert(7, css_code()).join
 
   end
   
--- svgstack.gemspec
+++ svgstack.gemspec
@@ -1,10 +1,11 @@
 Gem::Specification.new do |s|
   s.name = 'svgstack'
-  s.version = '0.1.1'
-  s.summary = 'A 1st attempt at creating a stack using SVG. Under development.'
+  s.version = '0.2.0'
+  s.summary = 'Creates a stack using SVG.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/svgstack.rb']
   s.add_runtime_dependency('victor', '~> 0.2', '>=0.2.6')
+  s.add_runtime_dependency('dynarex', '~> 1.8', '>=1.8.19')
   s.signing_key = '../privatekeys/svgstack.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
